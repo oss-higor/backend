6a55af7890cb6f89a901f61a334b16e06c306221
Higor Oliveira <oss.higor@gmail.com>
2019-04-11 13:56:26 -0300

    corrigindo o File.js

--- src/models/File.js
+++ src/models/File.js
@@ -18,7 +18,7 @@ const File = new mongoose.Schema({
 );
 
 File.virtual("url").get(function(){
-    const url = process.env.URL ||'http:localhost:4444'
+    const url = process.env.URL ||'http://localhost:4444'
     return `${url}/files/${encodeURIComponent(this.path)}` 
 })
 
